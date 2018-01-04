46d4bfa25d8152b84273dc950485662cc942af3b
Nikhil <nikhil@splunk.com>
2018-01-04 15:24:39 -0800

    brew audit passes

--- nova-cli.rb
+++ nova-cli.rb
@@ -1,18 +1,26 @@
-require 'formula'
-
 class NovaCli < Formula
-  homepage 'https://github.com/splunknova/nova-cli'
-  url 'https://github.com/splunknova/nova-cli.git', :branch => 'master'
+  desc "CLI tool to pipe logs to splunknova and search them"
+  homepage "https://github.com/splunknova/nova-cli"
 
-  depends_on 'go' => :build
+  stable do
+    url "https://github.com/splunknova/nova-cli.git", :tag => "v0.3.2"
+  end
+
+  devel do
+    url "https://github.com/splunknova/nova-cli.git", :tag => "v1.0.0"
+  end
+
+  depends_on "go" => :build
 
   def install
     ENV["GOROOT"] = "#{HOMEBREW_PREFIX}/opt/go/libexec"
     ENV["GOPATH"] = buildpath
+    (buildpath/"src/github.com/splunknova/nova-cli").install buildpath.children
     system "go", "env" # Debug env
-    system "go", "get", "github.com/splunknova/nova-cli"
-    system "go", "build", "nova.go"
-    bin.install 'nova'
+    cd buildpath/"src/github.com/splunknova/nova-cli" do
+      system "go", "build", "nova.go"
+      bin.install "nova"
+    end
   end
 
   test do
